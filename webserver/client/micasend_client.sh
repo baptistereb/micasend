7abb34414c0036e3198b358fcb80920071ab49ae
Baptiste <baptiste.rebillard@outlook.fr>
2022-11-19 12:32:16 +0100

    help

--- webserver/client/micasend_client.sh
+++ webserver/client/micasend_client.sh
@@ -29,6 +29,10 @@ function ParsingCommand() {
 	elif [ "$command" = "/delgame" ]
 	then
 		DelGame $2
+	elif [ "$command" = "/help" ]
+	then
+		AffHelp
+		read wait
 	elif [ "$command" = "/upuser" ]
 	then
 		UpUser $2 $3 $4
@@ -80,6 +84,33 @@ function UpUser() {
 	curl -s $host/upuser.php?user=$userup\&column=$column\&value=$value\&adminpseudo=$user\&admintoken=$token
 	echo -e "\n\n\\033[35mAfter\\033[0m"
 	ShowUser
+}
+
+function AffHelp() {
+	clear
+	echo -e "\\033[35m/showgame\\033[0m mid_player1 id_player2"
+	echo -e "\\033[33mPermet d'afficher la partie entre player1 et player2\\033[0m\n"
+
+	echo -e "\n\n\\033[31m\\033[01mModerator\\033[0m\n\n"
+	echo -e "\\033[35m/play\\033[0m id_de_votre_adversaire"
+	echo -e "\\033[33mPermet d'afficher votre partie en cours avec un adversaire et de jouer si c'est à votre tour\\033[0m\n"
+	echo -e "\\033[35m/delmsg\\033[0m id_message"
+	echo -e "\\033[33mPermet de supprimer un message\\033[0m\n"
+
+
+	echo -e "\n\n\\033[31m\\033[01mAdmin Only\\033[0m\n\n"
+	echo -e "\\033[35m/adduser\\033[0m pseudo rank(de 1 à 15)"
+	echo -e "\\033[33mPermet d'ajouter un utilisateur dans la base et retourne son token\\033[0m\n"
+	echo -e "\\033[35m/showuser\\033[0m"
+	echo -e "\\033[33mPermet d'afficher tout les utilisateurs et leurs parametres\\033[0m\n"
+	echo -e "\\033[35m/addgame\\033[0m id_player1 id_player2"
+	echo -e "\\033[33mPermet d'ajouter une partie entre player1 et player2\\033[0m\n"
+	echo -e "\\033[35m/delgame\\033[0m id_game"
+	echo -e "\\033[33mPermet de supprimer une partie\\033[0m\n"
+	echo -e "\\033[35m/upuser\\033[0m id_utilisateur colonne_à_modifier nouvelle_valeur"
+	echo -e "\\033[33mPermet de mettre à jour un utilisateur\\033[0m\n\n"
+
+	echo -e "\\033[05m\\033[31m\\033[01m/!\ les commandes ne retournent pas forcément d'erreur si certains critères ne sont pas respectés\\033[0m"
 }
 
 function DelMsg() {
