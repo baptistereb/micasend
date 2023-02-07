3bc1f8365932b726db5dc672dcaf6ec092957a32
Baptiste <baptiste.rebillard@outlook.fr>
2023-02-07 14:42:12 +0100

    on limite à 20msg

--- webserver/msg.php
+++ webserver/msg.php
@@ -21,7 +21,7 @@ if(isset($_GET['message']) AND !empty($_GET['message']) AND isset($_GET['sender'
         }
 	}
 
-	$reqins = $db->prepare("INSERT INTO msg(content, sender, id_certified_user, date_time) VALUES(?, ?, ?, ?)");
+	$reqins = $db->prepare("INSERT INTO msg(content, sender, id_certified_user, date_time) VALUES(?, ?, ?, ?) LIMIT 20");
 	$reqins->execute(array($msg, $sender, $certif, date("Y-m-d H:i:s", time())));
 	header('Location: msg.php');
 }
