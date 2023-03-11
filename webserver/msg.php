686014ebd9f6b5ef27973f71c9e563a8633047c7
baptistereb <baptiste.rebillard@outlook.fr>
2023-03-11 21:48:21 +0100

    fix: jt'ai patch Victor !!!! (merci chatGPT)

--- webserver/msg.php
+++ webserver/msg.php
@@ -21,7 +21,7 @@ if(isset($_GET['message']) AND !empty($_GET['message']) AND isset($_GET['sender'
         }
 	}
 
-	$msg = addcslashes($msg);
+	$msg = preg_replace('/[\x00-\x1F\x7F]/u', '', $msg);
 
 	$reqins = $db->prepare("INSERT INTO msg(content, sender, id_certified_user, date_time) VALUES(?, ?, ?, ?)");
 	$reqins->execute(array($msg, $sender, $certif, date("Y-m-d H:i:s", time())));
